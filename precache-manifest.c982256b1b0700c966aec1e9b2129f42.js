884495d852849371e64b9fc36e22bb57f79444ee
Bojan_S <bojanspasovski1@gmail.com>
2021-09-22 16:15:29 +0200

    initial deploy commit

---
+++ precache-manifest.c982256b1b0700c966aec1e9b2129f42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bde17acf1043d2001269",
+    "url": "/name-tag-generator-deploy/static/css/main.5b937dfa.chunk.css"
+  },
+  {
+    "revision": "bde17acf1043d2001269",
+    "url": "/name-tag-generator-deploy/static/js/main.2b56e97f.chunk.js"
+  },
+  {
+    "revision": "f0fd73551d8bc499ec97",
+    "url": "/name-tag-generator-deploy/static/js/runtime~main.84c18e11.js"
+  },
+  {
+    "revision": "45a7acfdace2e315d2d9",
+    "url": "/name-tag-generator-deploy/static/js/2.ee49d4bf.chunk.js"
+  },
+  {
+    "revision": "9ff5cd0cbede8a8d0ed23ea5190bfd83",
+    "url": "/name-tag-generator-deploy/index.html"
+  }
+];
---
+++ precache-manifest.cc36de8aeec4a0a2fbba8d986e8661bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bde17acf1043d2001269",
+    "url": "/name-tag-generator/static/css/main.5b937dfa.chunk.css"
+  },
+  {
+    "revision": "bde17acf1043d2001269",
+    "url": "/name-tag-generator/static/js/main.2b56e97f.chunk.js"
+  },
+  {
+    "revision": "3ae7b9e1728ccf5d7f50",
+    "url": "/name-tag-generator/static/js/runtime~main.0e060c98.js"
+  },
+  {
+    "revision": "45a7acfdace2e315d2d9",
+    "url": "/name-tag-generator/static/js/2.ee49d4bf.chunk.js"
+  },
+  {
+    "revision": "a1646484cda44a3e5ba3de09d36372ee",
+    "url": "/name-tag-generator/index.html"
+  }
+];
